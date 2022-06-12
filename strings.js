acd36ad3a63a8e459d7ac5f993425dd20554d590
Shamimgardobuya <shamimobuya@gmail.com>
2022-06-12 13:19:59 +0300

    used an endless for loop

---
+++ strings.js
@@ -0,0 +1,27 @@
+// //using the ? as a condition like the if statement
+// //declare it as either true or false.
+// let kids=23;
+//  let child=kids?23:false
+// console.log(child);
+// //had a way to write else condition 
+// //using for loops check check 
+// let student={
+//     name :"Conslate",
+//     age:23,
+//     class:"AnitaB"
+
+// }
+// delete student.name;
+// console.log(typeof(student));
+// console.log(student)
+// var today=new Date().toLocaleTimeString();
+// let t=(today instanceof Date)?true:false;
+// console.log(t)
+// console.log(typeof(today));
+let marks=89;
+let result=(marks>40)?"pass":"fail";
+console.log(`You have ${result} the exam`);
+let n =8;
+for(let x=1;x<n;x++){
+    console.log(x);
+}
